1b746b1c959004ae3c4cf31de07ca7235ec77086
miroslavradakovicRA402014 <miroslav.radakovic23@gmail.com>
2017-12-28 18:14:47 +0100

    Freq bug fixed

--- stream_controller.c
+++ stream_controller.c
@@ -412,6 +412,12 @@ void* streamControllerTask()
 	   return (void*) SC_ERROR;		
 	}
 	
+	if (config.configFreq != 754000000)
+	{
+	   printf("\nERROR Frequency doesn't exist!\n");		
+	   return (void*) SC_ERROR;		
+	}
+	
     /* lock to frequency */
     if(!Tuner_Lock_To_Frequency(config.configFreq, config.configBandwidth, config.configModule))
     {
